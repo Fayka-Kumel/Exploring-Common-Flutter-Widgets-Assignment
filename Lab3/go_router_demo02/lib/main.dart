3bf58f673aff5ce4e24b8ea16296275e5b6c5900
Fayka-K <fay67139@gmail.com>
2026-04-03 20:21:54 +0300

    adjust folder structure

--- Lab3/go_router_demo02/lib/main.dart
+++ Lab3/go_router_demo02/lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
 
   @override
   Widget build(BuildContext context) {
